b036e8d25a8191e62cbf60b825d0b2fba5edc3d7
djon260683 <djon260683@yandex.ru>
2021-06-21 00:04:38 +0500

    Подготовил Parcelable
    и добавил активити land

--- app/src/main/java/ru/eugen/calceugen/Calculations.java
+++ app/src/main/java/ru/eugen/calceugen/Calculations.java
@@ -1,4 +1,34 @@
 package ru.eugen.calceugen;
 
-public class Calculations {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+
+
+public class Calculations implements Parcelable {
+
+    protected Calculations(Parcel in) {
+    }
+
+    public static final Creator<Calculations> CREATOR = new Creator<Calculations>() {
+        @Override
+        public Calculations createFromParcel(Parcel in) {
+            return new Calculations(in);
+        }
+
+        @Override
+        public Calculations[] newArray(int size) {
+            return new Calculations[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        
+    }
 }
--- app/src/main/java/ru/eugen/calceugen/MainActivity.java
+++ app/src/main/java/ru/eugen/calceugen/MainActivity.java
@@ -1,14 +1,39 @@
 package ru.eugen.calceugen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.Serializable;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private final static String keyCalc = "Calc";
+    private Calculations calculations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle instanceState) {
+        super.onSaveInstanceState(instanceState);
+        instanceState.putSerializable(keyCalc, (Serializable) calculations);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle instanceState) {
+        super.onRestoreInstanceState(instanceState);
+        calculations = (Calculations) instanceState.getSerializable(keyCalc);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
